7dc361d838387e1273d8a54b26f67af103035186
anshuman <anshuman.si22@gmail.com>
2023-01-17 16:24:18 +0530

    airbnb data through props

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 import "./App.css";
+import data from "./data";
 
 function App() {
+  const dataFile = data.map((item) => {
+    return (
+      <Card
+        coverImg={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
+
+  console.log(dataFile);
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card
-        img="image1.png"
-        rating={5}
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {dataFile}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 function Card(props) {
-  console.log(props);
+  console.log("card" + props);
   return (
     <section className="card-container">
-      <img src={`${props.img}`} alt="card 1" className="card-img" />
+      <img src={`${props.coverImg}`} alt="card 1" className="card-img" />
       <div className="card-details">
         <div className="card-details_first">
           <img src="Star.png" alt="rating stars" className="star-img" />
@@ -12,7 +12,7 @@ function Card(props) {
           <span className="rating">{props.rating}</span>
           <span className="rating_no">&#40;{props.reviewCount}&#41;</span>
           <span className="dash">&#45;</span>
-          <span className="country">{props.country}</span>
+          <span className="country">{props.location}</span>
         </div>
         <div className="card-details_desc">
           <span className="trainer-detail">{props.title}</span>
